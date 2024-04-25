22efcef910e32e9d6f7bdb5bc59df0f3f38938e8
aYu <2426774691@qq.com>
2024-04-26 00:06:39 +0800

    from 2D to 3D  graph and shadow learning  by URP and the default rendering pipeline

---
+++ Assets/Scripts/Graph/FunctionLibrary.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using static UnityEngine.Mathf;
+public static class FunctionLibrary {
+
+    public static float Wave(float x, float z, float t) {
+        return Sin(PI * (x + t));
+        // return Mathf.Sin(Mathf.PI * (x + t));  使用了using static UnityEngine.Mathf; 无需显式说明方法和变量的类型
+    }
+
+    public static float MultiWave(float x, float z, float t)
+    {
+        float y = Sin(PI * (x + 0.5f * t));
+        // y += Sin(2f * PI * (x + t)) / 2f;
+        // return y / 1.5f;    // 使用除法在复杂计算的地方开销比乘法大，而简单的 1f/2f这样的常量表达式可以编译简化，而2/3无法十进制表示因此还是保持除法形式让编译器优化
+        y += Sin(2f * PI * (x + t)) * (1f / 2f);
+        return y * (2f / 3f);
+    }
+
+    public static float Ripple(float x, float z, float t)
+    {
+        float d = Abs(x);
+        float y = Sin(4f * PI * d - t);
+        return y / (1f + 10f * d);
+    }
+
+    public delegate float Function(float x, float z, float t);
+
+    public enum FunctionName { Wave, MultiWave, Ripple }
+    
+    static Function[] functions = { Wave, MultiWave, Ripple };
+
+    public static Function GetFunction(FunctionName name)
+    {
+        return functions[(int)name];
+    }
+}
--- Assets/Scripts/Graph/Graph.cs
+++ Assets/Scripts/Graph/Graph.cs
@@ -8,18 +8,29 @@ public class Graph : MonoBehaviour
     Transform pointPrefab;
     [SerializeField, Range(10,100)]
     int resolution = 10;
+    //[SerializeField, Range(0, 2)]
+    //int function;
+    [SerializeField]
+    FunctionLibrary.FunctionName function;
+
     Transform[] points;
     void Awake()
     {
         float step = 2f / resolution;
         var position = Vector3.zero;
         var scale = Vector3.one * step;
-        points = new Transform[resolution];
-        for (int i = 0; i < resolution; i++)
+        points = new Transform[resolution * resolution];
+        for (int i = 0, x = 0, z = 0; i < points.Length; i++, x++)
         {
+            if (x == resolution)
+            {
+                x = 0;
+                z++;
+            }
             Transform point = points[i] = Instantiate(pointPrefab);
-            position.x = (i + 0.5f) * step - 1f;
- 
+            position.x = (x + 0.5f) * step - 1f;
+            position.z = (z + 0.5f) * step - 1f;
+
             point.localPosition = position;
             point.localScale = scale;
             point.SetParent(transform,false);
@@ -36,12 +47,25 @@ public class Graph : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        FunctionLibrary.Function f = FunctionLibrary.GetFunction(function);
         float time = Time.time;
         for (int i = 0; i < points.Length; i++)
         {
             Transform point = points[i];
             Vector3 position = point.localPosition;
-            position.y = Mathf.Sin(Mathf.PI * (position.x + time));
+            //if (function == 0)
+            //{
+            //    position.y = FunctionLibrary.Wave(position.x, time);
+            //}
+            //else if(function == 1)
+            //{
+            //    position.y = FunctionLibrary.MultiWave(position.x, time);
+            //}
+            //else
+            //{
+            //    position.y = FunctionLibrary.Ripple(position.x, time);
+            //}
+            position.y = f(position.x, position.z, time);
             point.localPosition = position;
         }
     }
