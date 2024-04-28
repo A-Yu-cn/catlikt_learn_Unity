6e183e6581ec1947a4080cf345cadb82a4ec2ec8
aYu <2426774691@qq.com>
2024-04-29 00:19:56 +0800

    finish measure class and add TextMesh Pro to the project.

--- Assets/Scripts/Graph/Graph.cs
+++ Assets/Scripts/Graph/Graph.cs
@@ -14,6 +14,20 @@ public class Graph : MonoBehaviour
     FunctionLibrary.FunctionName function;
 
     Transform[] points;
+    
+    float duration;
+
+    [SerializeField, Min(0f)]
+    float functionDuration = 1f, transitionDuration = 1f;
+
+    bool transitioning;
+
+    FunctionLibrary.FunctionName transitionFunction;
+    public enum TransitionMode { Cycle, Random }
+
+    [SerializeField]
+    TransitionMode transitionMode;
+
     void Awake()
     {
         float step = 2f / resolution;
@@ -44,8 +58,35 @@ public class Graph : MonoBehaviour
         
     }
 
+
     // Update is called once per frame
     void Update()
+    {
+        duration += Time.deltaTime;
+        if (transitioning) {
+            if (duration >= transitionDuration)
+            {
+                duration -= transitionDuration;
+                transitioning = false;
+            }
+        }
+        else if (duration >= functionDuration)
+        {
+            duration -= functionDuration;
+            transitioning = true;
+            transitionFunction = function;
+            PickNextFunction();
+        }
+        if (transitioning)
+        {
+            UpdateFunctionTransition();
+        }
+        else
+        {
+            UpdateFunction();
+        }
+    }
+    void UpdateFunction()
     {
         FunctionLibrary.Function f = FunctionLibrary.GetFunction(function);
         float time = Time.time;
@@ -63,4 +104,36 @@ public class Graph : MonoBehaviour
             points[i].localPosition = f(u, v, time);
         }
     }
+    void PickNextFunction()
+    {
+        function = transitionMode == TransitionMode.Cycle ?
+            FunctionLibrary.GetNextFunctionName(function) :
+            FunctionLibrary.GetRandomFunctionNameOtherThan(function);
+    }
+
+    void UpdateFunctionTransition()
+    {
+        FunctionLibrary.Function
+            from = FunctionLibrary.GetFunction(transitionFunction),
+            to = FunctionLibrary.GetFunction(function);
+        float progress = duration / transitionDuration;
+        float time = Time.time;
+        float step = 2f / resolution;
+        float v = 0.5f * step - 1f;
+        for (int i = 0, x = 0, z = 0; i < points.Length; i++, x++)
+        {
+            if (x == resolution)
+            {
+                x = 0;
+                z += 1;
+                v = (z + 0.5f) * step - 1f;
+            }
+            float u = (x + 0.5f) * step - 1f;
+
+            points[i].localPosition = FunctionLibrary.Morph(
+                u, v, time, from, to, progress
+            );
+        }
+    }
+
 }
